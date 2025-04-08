27e5e06cb05eff01d9edb64318cd0126ef799a53
isaque <isaqueclaudino12@gmail.com>
2025-04-08 13:56:57 -0300

    fix(#12): adjusted sort import

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require __DIR__ . '/bootstrap.php';
-require __DIR__ . '/helpers.php';
 require_once __DIR__ . '/autoload.php';
+require __DIR__ . '/helpers.php';
 
 $router = new Router();
 $mysql = new Mysql();
